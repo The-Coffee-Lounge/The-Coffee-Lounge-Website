4c6e4f3ce49d3c472e02115a8d09509268c113f7
Adriane James J. Puzon <adrianejames27@gmail.com>
2022-04-17 15:42:47 +0000

    lots of progress XD

--- script.js
+++
@@ -1,8 +0,0 @@
-const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-// vw = vw - 10;
-const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
-console.log(vw);
-console.log(vh);
-var image_gallery = document.getElementById("image-gallery");
-image_gallery.style.width = vw+"px";
-image_gallery.style.height = vh+"px";
